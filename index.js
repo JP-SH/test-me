2894ec31fa059c52960fe5b84bb33c2c1151aa4f
Jasdeep Singh <j.singhhh917@gmail.com>
2025-05-30 18:28:38 +1000

    setting up breadth first search

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log('Running test!!....');
+const Runner = require('./runner');
+const runner = new Runner();
+
+const run = async () => {
+  const results = await runner.collectFiles(process.cwd());
+  // in the line above we want to collect the files from the correct path. Since we are going to be collecting files from the dir we are currently in. We use cwd which stands for 'current working directory'.
+  console.log(results);
+};
+
+run();
